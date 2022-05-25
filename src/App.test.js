152adf3b434b7a9cb31487006b9fa15523c69bd7
Katia <katia.punter@storyshareplatform.com>
2022-05-25 18:05:47 +0100

    Adds second failing assertion

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,8 @@ import App from './App';
 test("initial inputs are empty", ()=>{
   render(<App/>);
   const emailInput = screen.getByRole('textbox');
+  // there is no role called password
+  const passInput = screen.getByLabelText(/password/i);
   expect(emailInput.value).toBe('');
+  expect(passInput.value).toBe('');
 })
