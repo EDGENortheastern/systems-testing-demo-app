c5ab7333c3e3458ad1b3663947ffae8de42df785
Katia <katia.punter@storyshareplatform.com>
2022-05-25 18:10:55 +0100

    more comments to the second failing assertion

--- src/App.test.js
+++ src/App.test.js
@@ -4,7 +4,8 @@ import App from './App';
 test("initial inputs are empty", ()=>{
   render(<App/>);
   const emailInput = screen.getByRole('textbox');
-  // there is no role called password
+  // we need a more unique way to get password box
+  // there is no role password
   const passInput = screen.getByLabelText(/password/i);
   expect(emailInput.value).toBe('');
   expect(passInput.value).toBe('');
