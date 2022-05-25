960621f2aa50b0bf2250a670ca9345e4b666f1a7
Katia <katia.punter@storyshareplatform.com>
2022-05-25 20:14:20 +0100

    Adds third failing assertion

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,8 @@ test("initial inputs are empty", ()=>{
   const emailInput = screen.getByRole('textbox');
   // we need a more unique way to get password box
   // there is no role password
-  const passInput = screen.getByLabelText(/password/i);
+  const passInput = screen.getByLabelText("Password");
+  const confirmInput = screen.getByLabelText(/confirm password/i);
   expect(emailInput.value).toBe('');
   expect(passInput.value).toBe('');
 })
